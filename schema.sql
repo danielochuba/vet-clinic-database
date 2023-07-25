fba16b4adbd9a3b6b444cc5c346ea347fee0b0d9
Daniel Ochuba Ugochukwu <danielochuba78@gmail.com>
2023-07-25 02:46:53 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-id INT,
-name TEXT,
+id INT PRIMARY KEY NOT NULL,
+name VARCHAR(10),
 date_of_birth DATE,
 escape_attempts INT,
-neutered BIT,
+neutered BOOLEAN,
 weight_kg DECIMAL(8,2)
 );

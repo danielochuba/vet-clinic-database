d30dd7b98b9ef3f2f47b681b4f53d0eb5c4c6e2a
Daniel Ochuba Ugochukwu <danielochuba78@gmail.com>
2023-07-25 02:49:23 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,3 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals VALUES (1, 'Agumon', '2020-02-03', 0, B'1', 10.23), (2, 'Gabumon', '2018-11-15', 2, B'1', 8), (3, 'Pikachu', '2021-01-07', 1, B'0', 15.04), (4, 'Devimon', '2017-05-12', 5, B'1', 11);
+INSERT INTO animals VALUES (1, 'Agumon', '2020-02-03', 0, TRUE, 10.23), (2, 'Gabumon', '2018-11-15', 2, TRUE, 8), (3, 'Pikachu', '2021-01-07', 1, FALSE, 15.04), (4, 'Devimon', '2017-05-12', 5, TRUE, 11);

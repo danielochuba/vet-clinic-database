061a23fe034e78bad5ddcf37174c801476b63a66
Daniel Ochuba Ugochukwu <danielochuba78@gmail.com>
2023-08-09 17:11:58 +0100

    Add tables in the schema.sql file

--- schema.sql
+++ schema.sql
@@ -1,56 +1,47 @@
-/* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-id INT PRIMARY KEY NOT NULL,
-name VARCHAR(10),
-date_of_birth DATE,
-escape_attempts INT,
-neutered BOOLEAN,
-weight_kg DECIMAL(8,2)
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(20),
+  age INT,
+  PRIMARY KEY(id)
 );
 
-ALTER TABLE animals ADD COLUMN species VARCHAR(20);
-
-CREATE TABLE owners (
-id SERIAL PRIMARY KEY,
-full_name VARCHAR(100) NOT NULL,
-age INT NOT NULL
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE species (
-id SERIAL PRIMARY KEY,
-name VARCHAR(100)
+CREATE TABLE animals(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
+  species_id INT REFERENCES species(id),
+  owner_id INT REFERENCES owners(id),
+  PRIMARY KEY(id)
 );
 
-CREATE SEQUENCE animals_id_seq START 11;
-ALTER TABLE animals ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
-UPDATE animals SET id = nextval('animals_id_seq');
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
+);
 
-ALTER TABLE animals DROP COLUMN species;
+CREATE TABLE specializations(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  species_id INT REFERENCES species(id),
+  vet_id INT REFERENCES vets(id),
+  PRIMARY KEY(id)
+);
 
-ALTER TABLE animals ADD COLUMN species_id INT;
-ALTER TABLE animals ADD CONSTRAINT species_fkey FOREIGN KEY (species_id) REFERENCES species(id);
-
-ALTER TABLE animals ADD COLUMN owner_id INT;
-ALTER TABLE animals ADD CONSTRAINT owners_fkey FOREIGN KEY (owner_id) REFERENCES owners(id);
-
-CREATE TABLE vets (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  age INT NOT NULL,
-  date_of_graduation DATE
-  );
-
-CREATE TABLE specializations (
-  vets_id INT FOREIGN KEY REFERENCES vets(id),
-  species_id INT FOREIGN KEY REFERENCES species(id)
-  );
-
-CREATE TABLE visits (
-  animals_id INT,
-  vets_id INT,
-  date_of_visit DATE NOT NULL,
-  PRIMARY KEY (animals_id, vets_id, date_of_visit),
-  FOREIGN KEY (animals_id) REFERENCES animals(id),
-  FOREIGN KEY (vets_id) REFERENCES vets(id)
-  );
+CREATE TABLE visits(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  animal_id INT REFERENCES animals(id),
+  vet_id INT REFERENCES vets(id),
+  date_of_visit DATE,
+  PRIMARY KEY(id)
+);
